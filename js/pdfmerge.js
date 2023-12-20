fe5177dec5bfbf5d9eb689db49cbcb82e1b6d3e8
bradsec <github@bradsec.com>
2023-12-20 16:38:49 +1000

    Remove space

--- js/pdfmerge.js
+++ js/pdfmerge.js
@@ -381,7 +381,7 @@ function formatFileSize(bytes) {
   const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
   if (bytes == 0) return "0 Byte";
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-  return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
+  return Math.round(bytes / Math.pow(1024, i), 2) + "" + sizes[i];
 }
 
 function handleConversionTimeout() {
